3d0320d94e5f7a36b15d749acf11986050692a07
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2025-02-28 21:58:00 +0530

    Authentication Setup

--- src/models/server/comment.collection.ts
+++ src/models/server/comment.collection.ts
@@ -3,7 +3,7 @@ import { commentCollection, db } from "../name";
 import { databases } from "./config";
 
 export default async function createCommentCollection() {
-    // Creating Collection
+
     await databases.createCollection(db, commentCollection, commentCollection, [
         Permission.create("users"),
         Permission.read("any"),
@@ -13,7 +13,7 @@ export default async function createCommentCollection() {
     ]);
     console.log("Comment Collection Created");
 
-    // Creating Attributes
+
     await Promise.all([
         databases.createStringAttribute(db, commentCollection, "content", 10000, true),
         databases.createEnumAttribute(db, commentCollection, "type", ["answer", "question"], true),
